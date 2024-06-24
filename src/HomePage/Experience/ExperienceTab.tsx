64643fa7f2ed4af7940b15271785c82ab42ef78a
Nick Don <nick.don.1102@gmail.com>
2024-06-24 15:59:45 +1000

    add experience

---
+++ src/HomePage/Experience/ExperienceTab.tsx
@@ -0,0 +1,17 @@
+type ExperienceTabProps = {
+  title: string;
+  description: string[];
+};
+
+export default function ExperienceTab({ title, description }: ExperienceTabProps) {
+  return (
+    <div>
+      <h2 className="mb-3 mt-6 font-medium uppercase text-indigo-500">{title}</h2>
+      <ul className="list-inside list-disc text-gray-500 dark:text-gray-400">
+        {description.map((desc) => (
+          <li className="mb-3 text-sm font-light text-gray-500">{desc}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
