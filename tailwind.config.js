4f329a6c3b520cf7fe22984b28bac55a137044d8
Nick Don <nick.don.1102@gmail.com>
2024-07-15 12:57:23 +1000

    add tailwind config screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
   mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [daisyui],
