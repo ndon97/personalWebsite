698ab3f7008c873b953f37c8570ff1edb0e507af
Nick Don <nick.don.1102@gmail.com>
2024-07-15 13:40:28 +1000

    point updates

--- src/HomePage/Experience/Experience.tsx
+++ src/HomePage/Experience/Experience.tsx
@@ -12,9 +12,10 @@ export default function Experience() {
           role="Software Engineer"
           description="Xplor Technologies is the first global platform integrating Saas, Payments and Growth Technologies. They offer software solutions in fast-growing “everyday life” verticals: Childcare & Education, Fitness & Wellbeing, Field Services and Personal Services - and a global, cloud-based payment processing platform."
           points={[
-            'Led the frontend development of a comprehensive website redesign project.',
-            'Worked on major Typescript/React features that significantly enhanced UI/UX, increasing customer satisfaction scores by over 15%',
-            'Enhanced the Elixir backend API by adding new endpoints.',
+            'Led the frontend development of a comprehensive website redesign project. Creating over a 100 new components.',
+            'Worked on major Typescript/React features that significantly enhanced UI/UX, increasing customer satisfaction scores to over 97%.',
+            'Designed and enhanced multiple Elixir backend API’s by adding and updating new endpoints to support over 20+ new web features.',
+            'Developed and maintained several Elixir applications using Phoenix, Ecto, and ExUnit, resulting in a 15% increase in user engagement.',
             'Optimized existing GraphQL queries for better performance, eliminating a large number of customer complaints.',
             'Collaborated closely with product and design teams to develop an innovative, user-friendly product that meets customer needs.',
             'Identified, addressed, and resolved a range of bugs to improve overall website performance and stability.',
